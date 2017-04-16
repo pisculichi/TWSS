92e7ff7c376fcb08ac168898625c73fd1b2a9417
pisculichi <pisculichi@openmailbox.org>
2017-04-15 22:47:57 -0300

    Parsers microdata

---
+++ src/parsers/FandangoParser.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+import time
+import datetime
+import re
+import iso8601
+
+from models.Cinema import Cinema
+from models.Genre import Genre
+from models.Movie import Movie
+from models.Showtime import Showtime
+
+from bs4 import BeautifulSoup
+
+def remove_3d(title):
+	return re.sub('3D', '', title)
+
+def remove_year(title):
+	return re.sub(r'\([^)]*\)', '', title)
+
+def get_year(title):
+	if "(" in title:
+		return title[title.find("(")+1:title.find(")")]
+	else:
+		return ''
+
+class FandangoParser:
+
+	def __init__(self, sessiondb):
+		self.url = 'http://www.fandango.com/'
+		self.session = requests.session()
+		self.sessiondb = sessiondb
+
+	def fill_movies(self, soup):
+		movies_html = soup.findAll(attrs={'itemscope' : '', 'itemtype' : 'http://schema.org/Movie'})
+		for movie in movies_html:
+			title = movie.find(attrs={'itemprop':'name'})['content']
+			str_year = get_year(title)
+			if (len(str_year)>0):
+				year = int(str_year)
+			else:
+				year = None
+			title = remove_year(remove_3d(title)).strip()
+			s = self.sessiondb()
+			m = s.query(Movie).filter(Movie.title == title).first()
+			if not m:
+				m = Movie(title=title,year=year)
+			s.add(m)
+			s.commit()
+			s.close()
+
+	def fill_movie_theatre_event(self, soup, cinema):
+		events = soup.findAll(attrs={'itemprop' : 'event', 'itemtype' : 'http://schema.org/TheaterEvent'})
+		for event in events:
+			title = event.find(attrs={'itemprop':'name'})['content']
+			s = self.sessiondb()
+			cinema = s.merge(cinema)
+			if "3D" in title:
+				room = "3D"
+			else:
+				room = "2D"
+			title = remove_year(remove_3d(title)).strip()
+			movie = s.query(Movie).filter(Movie.title == title).first()			
+			dates = event.findAll(attrs={'itemprop':'startDate'})
+			for date in dates:
+				d = date['content']
+				d = iso8601.parse_date(d)
+				showtime = Showtime(date=d, room=room, cinema=cinema, movie=movie)
+				s.add(showtime)
+				s.commit()
+			s.close()
+
+	def fill_movie_theatre(self, soup):
+		movie_theatres = soup.findAll(attrs={'itemscope' : '', 'itemtype' : 'http://schema.org/MovieTheater'})
+		for movie_theatre in movie_theatres:
+			name = movie_theatre.find(attrs={'itemprop':'name'})['content']
+			cinema = Cinema(name=name)
+			s = self.sessiondb()
+			s.add(cinema)
+			s.commit()
+			s.close()
+			self.fill_movie_theatre_event(movie_theatre, cinema)
+
+	def fill_db(self, url_movie_theatre):
+		html = self.session.get(self.url + url_movie_theatre)
+		soup = BeautifulSoup(html.text, 'lxml')
+		self.fill_movies(soup)
+		self.fill_movie_theatre(soup)
+
---
+++ src/parsers/ImdbParser.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+import re
+import json
+
+from models.Genre import Genre
+from models.Movie import Movie
+
+from bs4 import BeautifulSoup
+
+class ImdbParser:
+
+	def __init__(self, sessiondb):
+		self.url = 'http://www.imdb.com/'
+		self.session = requests.session()
+		self.sessiondb = sessiondb
+
+	def get_url_imdb_movie(self, movie):
+		year = movie.year
+		if year:
+			year_str=str(year)
+		else:
+			year_str=''
+		resp = requests.get('http://www.omdbapi.com/?t='+movie.title+'&y='+year_str).text
+		imdb_data = json.loads(resp)
+		return self.url + 'title/' + imdb_data['imdbID']
+
+	def fill_movie(self, url_imdb, movie):
+		html = requests.get(url_imdb, headers={"Accept-Language": "en-US,en;q=0.5"})
+		soup = BeautifulSoup(html.text, 'lxml')
+		movie_html = soup.find(attrs={'itemscope' : '', 'itemtype' : 'http://schema.org/Movie'})
+		if movie_html:
+			s = self.sessiondb()
+			movie = s.merge(movie)
+			duration = movie_html.find(attrs={'itemprop':'duration'})['datetime']
+			movie.duration = int(re.sub('[a-zA-Z]', '', duration))
+			movie.calification = movie_html.find(attrs={'itemprop':'contentRating'})['content']
+			genders = movie_html.findAll(attrs={'itemprop':'genre'})
+			for genre in genders:
+				g = genre.text
+				gen = s.query(Genre).filter(Genre.name == g).first()
+				if not gen:
+					gen = Genre(name=g)
+					s.add(gen)
+				movie.genders.append(gen)
+			director_html = movie_html.find(attrs={'itemprop' : 'director', 'itemtype' : 'http://schema.org/Person'})
+			director = director_html.find(attrs={'itemprop' : 'name'}).text.strip()
+			year = movie_html.find(attrs={'itemprop' : 'datePublished'})['content'].split('-')[0]
+			movie.director = director
+			movie.year = int(year)
+			movie.synopsis = movie_html.find(attrs={'itemprop' : 'description'}).text.strip()
+			s.commit()
+			s.close
+		else:
+			print('No se ha podido obtener información de la api omdbapi')
+			print(movie)
+
+	def fill_db(self):
+		s = self.sessiondb()
+		movies = s.query(Movie).all()
+		s.close()
+		for movie in movies:
+			url_imdb_movie = self.get_url_imdb_movie(movie)
+			self.fill_movie(url_imdb_movie, movie)
+			
